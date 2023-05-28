395cadb13c67afbce676d7993b0c5198cba7b37b
KSSidll <kssidll@users.noreply.github.com>
2023-05-28 15:27:03 +0200

    Improve ShowConnectionsScreen UI

--- app/src/main/java/com/kssidll/choochoo/ui/showconnections/ShowConnectionsScreen.kt
+++ app/src/main/java/com/kssidll/choochoo/ui/showconnections/ShowConnectionsScreen.kt
@@ -1,17 +1,22 @@
 package com.kssidll.choochoo.ui.showconnections
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,10 +24,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kssidll.choochoo.ui.theme.ChooChooTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @Composable
 fun ShowConnectionsScreen(
@@ -37,13 +46,11 @@ fun ShowConnectionsScreen(
             destination = destination,
             onBackClick = onBackClick
         )
-        Box(modifier = Modifier.padding(horizontal = 12.dp)) {
-            ShowConnectionsScreenContent(
-                origin = origin,
-                destination = destination,
-                onConnectionSelect = onConnectionSelect
-            )
-        }
+        ShowConnectionsScreenContent(
+            origin = origin,
+            destination = destination,
+            onConnectionSelect = onConnectionSelect
+        )
     }
 }
 
@@ -89,31 +96,60 @@ fun ShowConnectionsScreenAppBar(
     }
 }
 
+@SuppressLint("SimpleDateFormat")
 @Composable
 fun ShowConnectionsScreenContent(
     origin: String,
     destination: String,
     onConnectionSelect: (ConnectionData) -> Unit
 ) {
-    val data = generateConnections(
+    val connectionsData = generateConnections(
         amount = 3,
         origin = origin,
         destination = destination
     )
-    Column {
-        for (item in data) {
-            Column {
-                Text(text = "${item.origin} - origin")
-                Text(text = "${item.destination} - destination")
-                Text(text = "${item.timeDeparture.toString().substring(0,5)} - timeDeparture")
-                Text(text = "${item.timeArrival.toString().substring(0,5)} - timeArrival")
+
+    LazyColumn {
+        for (i in 0..30) {
+            this.item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                        .background(MaterialTheme.colorScheme.primary)
+                ) {
+                    Row(
+                        horizontalArrangement = Center,
+                        verticalAlignment = CenterVertically,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        val currentTime = Calendar.getInstance().time
+                        val time = currentTime.time.plus(i*1000*60*60*24)
+                        val formatter = SimpleDateFormat("dd MMM", Locale.ENGLISH)
+                        Text(text = formatter.format(time).toString())
+                    }
+                }
             }
-            Spacer(modifier = Modifier.height(5.dp))
+            for (connection in connectionsData) {
+                this.item {
+                    Column {
+                        Box(modifier = Modifier.clickable { onConnectionSelect(connection) }) {
+                            ConnectionItem(
+                                connectionData = connection,
+                                modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+                            )
+                        }
+                        if (connection != connectionsData.last()) {
+                            Divider()
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
-@Preview(group = "ShowConnectionsScreen", name = "Show Connections Light", apiLevel = 29, showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+//@Preview(group = "ShowConnectionsScreen", name = "Show Connections Light", apiLevel = 29, showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Preview(group = "ShowConnectionsScreen", name = "Show Connections Dark", apiLevel = 29, showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun ShowConnectionsScreenPreview() {
@@ -123,8 +159,8 @@ fun ShowConnectionsScreenPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             ShowConnectionsScreen(
-                origin = "test",
-                destination = "test2",
+                origin = "origin",
+                destination = "destination",
                 onConnectionSelect = {},
                 onBackClick = {}
             )
